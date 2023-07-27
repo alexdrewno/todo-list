0f47ad2b2eaf83d4c0ac5d6cce1d3087483741d9
Alex <adrewno14@gmail.com>
2023-07-27 18:06:55 -0500

    fixed base url

--- vite.config.ts
+++ vite.config.ts
@@ -4,10 +4,10 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [react()],
+    base: '/todo-list/',
     test: {
         globals: true,
         environment: 'happy-dom',
         setupFiles: './src/config/setupTests.ts',
-        base: '/todo-list/',
     },
 })
