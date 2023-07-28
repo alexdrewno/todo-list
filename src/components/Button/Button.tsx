ed6e6096441e69dae7293e88ce7c897f434c0e03
Alex <adrewno14@gmail.com>
2023-07-27 20:35:08 -0500

    updated delete button styling

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -4,11 +4,16 @@ type ButtonProps = {
     title?: string
     onClick?: React.MouseEventHandler<HTMLButtonElement>
     type?: 'button' | 'submit' | 'reset' | undefined
+    className?: string
 }
 
-export function Button({ title, onClick, type }: ButtonProps) {
+export function Button({ title, onClick, type, className }: ButtonProps) {
     return (
-        <button className={styles.button} onClick={onClick} type={type}>
+        <button
+            className={`${styles.button} ${className}`}
+            onClick={onClick}
+            type={type}
+        >
             {title}
         </button>
     )
